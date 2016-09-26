125c732508403f64322d5cc108c6c0f8ea5dd368
making <makingx@gmail.com>
2016-09-26 23:11:00 +0900

    Add

---
+++ eureka-server/src/main/java/mrs/EurekaServerApplication.java
@@ -0,0 +1,12 @@
+package mrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
